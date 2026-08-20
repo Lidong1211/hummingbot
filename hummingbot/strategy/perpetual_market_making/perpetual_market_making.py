75f2b6f5cb1362124a3f5e060068177e5e93bff6
Lidong <lidong_1211@163.com>
2026-08-20 13:10:57 +0800

    maker for test

--- hummingbot/strategy/perpetual_market_making/perpetual_market_making.py
+++ hummingbot/strategy/perpetual_market_making/perpetual_market_making.py
@@ -482,35 +482,32 @@ class PerpetualMarketMakingStrategy(StrategyPyBase):
                     self.logger().warning("WARNING: Some markets are not connected or are down at the moment. Market "
                                           "making may be dangerous when markets or networks are unstable.")
 
-            if len(session_positions) == 0:
-                self._exit_orders = dict()  # Empty list of exit order at this point to reduce size
-                proposal = None
-                if self._create_timestamp <= self.current_timestamp:
-                    # 1. Create base order proposals
-                    proposal = self.create_base_proposal()
-                    self.logger().debug(f"Initial proposals: {proposal}")
-                    # 2. Apply functions that limit numbers of buys and sells proposal
-                    self.apply_order_levels_modifiers(proposal)
-                    self.logger().debug(f"Proposals after order level modifier: {proposal}")
-                    # 3. Apply functions that modify orders price
-                    self.apply_order_price_modifiers(proposal)
-                    self.logger().debug(f"Proposals after order price modifiers: {proposal}")
-                    # 4. Apply budget constraint, i.e. can't buy/sell more than what you have.
-                    self.apply_budget_constraint(proposal)
-                    self.logger().debug(f"Proposals after budget constraints: {proposal}")
+            self._exit_orders = dict()  # Empty list of exit order at this point to reduce size
+            proposal = None
+            if self._create_timestamp <= self.current_timestamp:
+                # 1. Create base order proposals
+                proposal = self.create_base_proposal()
+                self.logger().debug(f"Initial proposals: {proposal}")
+                # 2. Apply functions that limit numbers of buys and sells proposal
+                self.apply_order_levels_modifiers(proposal)
+                self.logger().debug(f"Proposals after order level modifier: {proposal}")
+                # 3. Apply functions that modify orders price
+                self.apply_order_price_modifiers(proposal)
+                self.logger().debug(f"Proposals after order price modifiers: {proposal}")
+                # 4. Apply budget constraint, i.e. can't buy/sell more than what you have.
+                self.apply_budget_constraint(proposal)
+                self.logger().debug(f"Proposals after budget constraints: {proposal}")
 
-                    self.filter_out_takers(proposal)
-                    self.logger().debug(f"Proposals after takers filter: {proposal}")
+                self.filter_out_takers(proposal)
+                self.logger().debug(f"Proposals after takers filter: {proposal}")
 
-                self.cancel_active_orders(proposal)
-                self.cancel_orders_below_min_spread()
-                if self.to_create_orders(proposal):
-                    self.execute_orders_proposal(proposal, PositionAction.OPEN)
-                # Reset peak ask and bid prices
-                self._ts_peak_ask_price = market.get_price(self.trading_pair, False)
-                self._ts_peak_bid_price = market.get_price(self.trading_pair, True)
-            else:
-                self.manage_positions(session_positions)
+            self.cancel_active_orders(proposal)
+            self.cancel_orders_below_min_spread()
+            if self.to_create_orders(proposal):
+                self.execute_orders_proposal(proposal, PositionAction.OPEN)
+            # Reset peak ask and bid prices
+            self._ts_peak_ask_price = market.get_price(self.trading_pair, False)
+            self._ts_peak_bid_price = market.get_price(self.trading_pair, True)
         finally:
             self._last_timestamp = timestamp
 
